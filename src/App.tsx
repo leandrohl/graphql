45296fff538b43d4fcdb84b0738af2d5a71a3263
LucasAnitelli <lucas.anitelli@marttech.com.br>
2021-08-20 08:31:53 -0300

    colocando tela de mission

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 
-import './App.css'
-// import Dragon from './pages/Dragon'
-import Users from './pages/Users'
+import './App.css';
+import Dragon from './pages/Dragon';
+import Users from './pages/Users';
+import Mission from './pages/Mission';
 
 function App() {
 
   return (
     <div>
       <Dragon />
+      <Mission />
       <Users />
     </div>
   );
